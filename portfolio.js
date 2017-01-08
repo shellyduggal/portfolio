25b0a6775c5a03bc8c393ab42a95d5f750a7aab6
Shelly Duggal <duggalsf@gmail.com>
2017-01-08 13:25:41 -0600

    initial commit

--- portfolio.js
+++ portfolio.js
@@ -5,3 +5,12 @@ $(function() {
 		height:'auto'
 	});
 });
+
+//redirect from thankyou page
+$(document).ready(function() {
+	setTimeout(function() {
+		$("#thanks").fadeOut(2000, function() {
+			window.location.replace("http://www.21beans.com");
+		});
+	}, 2000);
+});
