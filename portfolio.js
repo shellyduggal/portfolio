4793d86baac21bf37d3b6e09fb2054e1189e8aaa
Shelly Duggal <shellduggal@gmail.com>
2017-01-12 15:44:19 -0600

    Update Message Sent Page Design

--- portfolio.js
+++ portfolio.js
@@ -9,7 +9,7 @@ $(function() {
 //redirect from thankyou page
 $(document).ready(function() {
 	setTimeout(function() {
-		$("#thanks").fadeOut(2000, function() {
+		$("#intro").fadeOut(2500, function() {
 			window.location.replace("http://www.21beans.com");
 		});
 	}, 2000);
