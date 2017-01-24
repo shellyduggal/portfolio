4268bd897e4716b59f0c95d92033e325be9eb56b
Shelly Duggal <shellduggal@gmail.com>
2017-01-24 11:28:13 -0600

    Add PostCSS and refactor

--- app/app.js
+++ app/app.js
@@ -2,7 +2,7 @@
 //redirect from thankyou page
 $(document).ready(function() {
 	setTimeout(function() {
-		$("#intro").fadeOut(2500, function() {
+		$("#thanks").fadeOut(2500, function() {
 			window.location.replace("http://www.21beans.com");
 		});
 	}, 2000);
